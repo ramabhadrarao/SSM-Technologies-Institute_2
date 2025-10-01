5c4a0f924e1fc3b6a7ec22566f503f990a9c26a9
ramabhadrarao <ramabhadrarao.maddu@yahoo.com>
2025-10-01 17:26:39 +0530

    committed

--- src/pages/Admin/TeamManagement.tsx
+++ src/pages/Admin/TeamManagement.tsx
@@ -403,8 +403,8 @@ const TeamManagement: React.FC = () => {
                 <option value="inactive">Inactive</option>
               </select>
               <select
-              value={filterDepartment}
-              onChange={(e) => setFilterDepartment(e.target.value)}
+              value={departmentFilter}
+              onChange={(e) => setDepartmentFilter(e.target.value)}
               className="px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             >
               <option value="">All Departments</option>
